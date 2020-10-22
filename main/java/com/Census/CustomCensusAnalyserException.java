79c64fc7fc46f325badcb313db61425d82b17223
suyash-dce <jain.suyash28@gmail.com>
2020-10-22 15:48:07 +0530

    Added: Exception Class for Delimiter Exception Handling is Added

---
+++ main/java/com/Census/CustomCensusAnalyserException.java
@@ -0,0 +1,10 @@
+public class CustomCensusAnalyserException extends Exception {
+	enum ExceptionType {
+		IncorrectCsvFile, IncorrectData;
+	}
+	ExceptionType type;
+	public CustomCensusAnalyserException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
