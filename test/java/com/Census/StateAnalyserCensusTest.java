4c5b818fc19931e76efd065cc286d67b80a5f4b1
suyash-dce <jain.suyash28@gmail.com>
2020-10-22 15:42:14 +0530

    Modified: Test File for File Type Exception Handling is Modified

--- test/java/com/Census/StateAnalyserCensusTest.java
+++ test/java/com/Census/StateAnalyserCensusTest.java
@@ -1,14 +1,33 @@
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class StateAnalyserCensusTest {
 	public static final String STATE_CENSUS_DATA_FILE = "F:\\Capgemini_training1\\java_eclipse\\IndiaStateCensusAnalyser"
 			+ "\\IndiaStateCensusData.csv";
+	public static final String WRONG_STATE_CENSUS_DATA_FILE = "F:\\Capgemini_training1\\java_eclipse\\IndiaStateCensusAnalyser"
+			+ "\\IndiaStateCensusData.cv";
+	public static final String WRONG_DATA_IN_CENSUS_FILE = "F:\\Capgemini_training1\\java_eclipse\\IndiaStateCensusAnalyser\\IndiaStateCensusDataDelimiter.csv";
 
 	@Test
-	public void testSomeLibraryMethod() {
+	public void whenNumberOfRecordMatchesShouldPassTest() throws CustomCensusAnalyserException, IOException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-		int entries = stateCensusAnalyser.loadCsvData(STATE_CENSUS_DATA_FILE);
-		Assert.assertEquals(29, entries);
+		int totalEntriesInCsvFile = stateCensusAnalyser.loadCsvData(STATE_CENSUS_DATA_FILE);
+		Assert.assertEquals(29, totalEntriesInCsvFile);
+	}
+
+	@Test
+	public void whenFileIsNotInCsvFormatShouldThrowCustomException() throws CustomCensusAnalyserException, IOException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		int totalEntriesInCsvFile = stateCensusAnalyser.loadCsvData(WRONG_STATE_CENSUS_DATA_FILE);
+		Assert.assertEquals(29, totalEntriesInCsvFile);
+	}
+
+	@Test
+	public void whenFileDataIsImproperShouldThrowCustomException() throws CustomCensusAnalyserException, IOException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		int totalEntriesInCsvFile = stateCensusAnalyser.loadCsvData(WRONG_DATA_IN_CENSUS_FILE);
+		Assert.assertEquals(29, totalEntriesInCsvFile);
 	}
 }
