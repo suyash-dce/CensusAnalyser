a1fc3de915f2af9b8329e3d5c876dc9899348a6e
suyash-dce <jain.suyash28@gmail.com>
2020-10-22 16:03:01 +0530

    Added: Exception Class For Exception Handling in State Codes

---
+++ main/java/com/Census/CustomCensusAnalyserException.java
@@ -0,0 +1,12 @@
+public class CustomCensusAnalyserException extends Exception {
+	enum ExceptionType {
+
+		IncorrectCsvFile, IncorrectData, NO_DATA;
+
+	}
+	ExceptionType type;
+	public CustomCensusAnalyserException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
